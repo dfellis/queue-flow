8ac84aee682ff33c4e6f07d692df1242538d871c
David Ellis <d.f.ellis@ieee.org>
2012-07-20 00:17:13 -0700

    Some more tests helped eliminate some infinite recursion bugs, missing s on , fixed bugs with the  method, etc. Also removed an early testing method not desirable for the real API and cleaned up some conditionals a bit.

--- test/test.js
+++ test/test.js
@@ -54,3 +54,55 @@ exports.filter = function(test) {
 		test.done();
 	}).closeOnEmpty();
 };
+
+exports.on = function(test) {
+	test.expect(5);
+	q([1, 2, 3])
+		.on('close', function() {
+			test.ok(true, 'close event fired');
+			test.done();
+		})
+		.on('pull', function() {
+			test.ok(true, 'pull event fired');
+		})
+		.on('empty', function() {
+			test.ok(true, 'empty event fired');
+		})
+		.closeOnEmpty()
+		.toArray(function() { });
+	q([1, 2, 3])
+		.closeOnEmpty()
+		.on('close', function() {
+			return false;
+		})
+		.toArray(function() {
+			test.ok(false, 'array method never executes final callback');
+		});
+};
+
+exports.branch = function(test) {
+	test.expect(3);
+	q([1, 2, 'skip a few', 99, 100]).branch(function(value) {
+		if(value == value + 0) {
+			return value > 50 ? 'big' : 'small';
+		} else {
+			return 'invalid';
+		}
+	});
+	var num = 0;
+	q('big').toArray(function(result) {
+		test.equal([99, 100].toString(), result.toString(), 'big queue properly populated');
+		num++;
+		if(num == 3) test.done();
+	}).closeOnEmpty();
+	q('small').toArray(function(result) {
+		test.equal([1, 2].toString(), result.toString(), 'small queue properly populated');
+		num++;
+		if(num == 3) test.done();
+	}).closeOnEmpty();
+	q('invalid').toArray(function(result) {
+		test.equal(['skip a few'].toString(), result.toString(), 'invalid queue properly populated');
+		num++;
+		if(num == 3) test.done();
+	}).closeOnEmpty();
+};

fc28aeb7d44cc78f3f5d529eab2e82fcb4d6010f
David Ellis <d.f.ellis@ieee.org>
2012-08-14 21:12:20 -0700

    Adding a helper method to convert an object into an array of tuples, useful for processing individual key-value pairs separately in a queue-flow. Inspired by functionality in a similar library called BatchFlow.

--- test/test.js
+++ test/test.js
@@ -214,3 +214,11 @@ exports.each = function(test) {
 			test.done();
 		});
 };
+
+exports.tuple = function(test) {
+	test.expect(1);
+	test.equal(q.tuple({
+		a: 'b', c: 'd', e: 'f', g: 'h', i: 'j', k: 'l', m: 'n', o: 'p', q: 'r', s: 't', u: 'v', w: 'x', y: 'z'
+	}).length, 13, 'tuple properly converts object into array of key-value tuples');
+	test.done();
+};

88190e34f698910d187380971f13fc0f31554423
David Ellis <d.f.ellis@ieee.org>
2013-06-20 17:00:48 -0700

    Bump async-cancelable-events version and switch to my own fork of jscoverage.

--- test/test-node.js
+++ test/test-node.js
@@ -12,33 +12,10 @@ for(var key in tests) {
 exports.jscoverage = function(test) {
     test.expect(1);
     jscoverage.coverageDetail();
-    // Copied directly from jscoverage and edited, since getting at these values directly isn't possible
-    var file;
-    var tmp;
-    var total;
-    var touched;
-    var n, len;
-    if (typeof global._$jscoverage === 'undefined') {
-        return;
-    }
-    var lcov = "";
-    Object.keys(global._$jscoverage).forEach(function(key) {
-        file = key;
-        lcov += "SF:" + file + "\n";
-        tmp = global._$jscoverage[key];
-        if (typeof tmp === 'function' || tmp.length === undefined) return;
-        total = touched = 0;
-        for (n = 0, len = tmp.length; n < len; n++) {
-            if (tmp[n] !== undefined) {
-                lcov += "DA:" + n + "," + tmp[n] + "\n";
-                total ++;
-                if (tmp[n] > 0)
-                    touched ++;
-            }
-        }
-        test.equal(total, touched, 'All lines of code exercised by the tests');
+    var coverageStats = jscoverage.coverageStats();
+    Object.keys(coverageStats).forEach(function(file) {
+        test.equal(coverageStats[file].total, coverageStats[file].touched, 'All lines of code exercised by the tests');
     });
-    lcov += "end_of_record\n";
-    if(process.env.TRAVIS) coveralls.handleInput(lcov);
+    if(process.env.TRAVIS) coveralls.handleInput(jscoverage.getLCOV());
     test.done();
 };

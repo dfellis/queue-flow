a5a2fe2c421fa30c73bd31bff8fa86a54a5f1acb
David Ellis <d.f.ellis@ieee.org>
2013-10-04 21:37:01 -0700

    Fix browserling tests since fs isn't implemented there

--- test/test-browser.js
+++ test/test-browser.js
@@ -5,7 +5,11 @@ var perfTest = require('./test-perf');
 tests.getObjs(q, {});
 
 for(var key in tests) {
-    if(key !== 'getObjs' && key !== 'flattenAndNode' && key !== 'readStreams' && key !== 'complexity') tape(key, tests[key]);
+    if (key !== 'getObjs' &&
+        key !== 'flattenAndNode' &&
+        key !== 'readStreams' &&
+        key !== 'pipe' &&
+        key !== 'complexity') tape(key, tests[key]);
 }
 
 for(var key in perfTest) {

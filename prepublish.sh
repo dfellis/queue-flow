1dac4db4bf74d8f716d1639e809f2d96e636c04b
David Ellis <d.f.ellis@ieee.org>
2013-10-10 18:39:29 -0700

    fixing prepublish script issue

--- prepublish.sh
+++ prepublish.sh
@@ -8,7 +8,7 @@ git checkout gh-pages
 rm -rf docs
 mv docs-new docs
 git commit -am "Automatic documentation for version $npm_package_version"
-git checkout master
+git checkout 0.6.x
 git stash pop
 browserify ./lib/browserify.js | uglifyjs > ./lib/queue-flow.min.js
 git commit -am "Automatic minification for version $npm_package_version"
